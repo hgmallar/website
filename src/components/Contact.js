7ed7702694d30a383dd6ca833c9b064736eb8c2b
Hilary Mallar <hilarymallar@gmail.com>
2020-11-17 10:07:23 -0500

    Updated portfolio

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,36 +6,38 @@ import resume from "./../documents/HilaryMallar_Resume.pdf";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFile } from "@fortawesome/free-solid-svg-icons";
 
-const Contact = (props) => (
-  <div id="container">
-    <Header page="contact" />
-    <Title title={props.title} />
-    <div className="row justify-content-center width">
-      <div id="content" className="col-md-10 bg-white">
-        <p className="text-center">
-          <span className="font-weight-bold">email: </span>
-          <a className="contact" href="mailto:hilarymallar@gmail.com">
-            hilarymallar@gmail.com
-          </a>
-        </p>
-        <p className="text-center">
-          <span className="font-weight-bold">phone: </span>(315) 254-8151
-        </p>
-        <p className="text-center">
-          <a
-            className="contact"
-            href={resume}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faFile} size="lg" className="mr-1" />
-            Resume
-          </a>
-        </p>
+const Contact = (props) => {
+  return (
+    <div id="container">
+      <Header page="contact" />
+      <Title title={props.title} />
+      <div className="row justify-content-center width">
+        <div id="content" className="col-md-10 bg-white">
+          <p className="text-center">
+            <span className="font-weight-bold">email: </span>
+            <a className="contact" href="mailto:hilarymallar@gmail.com">
+              hilarymallar@gmail.com
+            </a>
+          </p>
+          <p className="text-center">
+            <span className="font-weight-bold">phone: </span>(315) 254-8151
+          </p>
+          <p className="text-center">
+            <a
+              className="contact"
+              href={resume}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faFile} size="lg" className="mr-1" />
+              Resume
+            </a>
+          </p>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Contact;
 
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,9 +3,8 @@ import PropTypes from "prop-types";
 import Header from "./Header.js";
 import Title from "./Title.js";
 import baby from "./../images/baby.PNG";
-import foodTruck from "./../images/food-truck.png";
 import youthRally from "./../images/frontpage.PNG";
-import ticTacToe from "./../images/gamepage.PNG";
+import voteGeorgia from "./../images/georgia.PNG";
 import ImageCard from "./ImageCard";
 
 const Portfolio = (props) => (
@@ -19,7 +18,7 @@ const Portfolio = (props) => (
         link="https://www.unpopularbaby.com/"
         summary="Full stack React web application to search for names using
                   popularity by decade and/or letter placements."
-        code="React.js, SQL, Express, Node, Sequelize, HTML, Flexbox,
+        code="React.js, Chart.js, SQL, Node, Express, Axios, Sequelize, HTML, Flexbox,
                   FontAwesome, and Google Fonts. Responsive Bootstrap design."
         codeLink="https://github.com/hgmallar/uncommonBaby"
       />
@@ -29,34 +28,20 @@ const Portfolio = (props) => (
         src={youthRally}
         link="https://www.youthrally.org/"
         summary="React web application for non-profit with custom store."
-        code="Node, Express, React.js, HTML, CSS, Javascript,
+        code="React.js, HTML, CSS, Javascript,
             FontAwesome, and Google Fonts styling. Responsive Bootstrap
             design."
         codeLink="https://github.com/hgmallar/youth-rally"
       />
 
       <ImageCard
-        title="Food Truck Map"
-        src={foodTruck}
-        link="https://evening-brushlands-53491.herokuapp.com/"
-        summary="A group project, full stack React web application to
-            communicate in real time between food trucks and customers."
-        code="React.js, Mongo, Express, Node, Passport, Mongoose,
-            Axios, Socket.io, HTML, Flexbox, and Leaflet. Responsive
+        title="How To Vote Georgia"
+        src={voteGeorgia}
+        link="https://howtovotegeorgia.com/"
+        summary="Non-partisan, independent Full stack React web application to help voters get clear, concise and up-to-date information on voting in the Georgia runoff."
+        code="React.js, Node, Express, Axios, HTML, CSS, and Javascript. Responsive Material UI
             design."
-        codeLink="https://github.com/hgmallar/WMFA"
-      />
-
-      <ImageCard
-        title="Tic Tac Toe"
-        src={ticTacToe}
-        link="https://infinite-taiga-34074.herokuapp.com/"
-        summary="A group project, full stack tic tac toe web application that
-            tracks user scores in a database."
-        code="Socket.io, MySQL, Node, Express, Handlebars, Sequelize,
-            Javascript, jQuery, Chart.js, HTML, and CSS. Responsive
-            Flexbox design."
-        codeLink="https://github.com/hgmallar/project2"
+        codeLink="https://github.com/hgmallar/georgia"
       />
     </div>
   </div>
